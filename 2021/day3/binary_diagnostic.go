ca2aa7b63f64639e1bfe79b8ef522df51777bbe0
Lidiya Ribakova <lida.ribakova@gmail.com>
2021-12-06 20:41:07 +0100

    day3 refactoring

--- 2021/day3/binary_diagnostic.go
+++ 2021/day3/binary_diagnostic.go
@@ -30,6 +30,57 @@ func main() {
 	fmt.Println("life support rating:", lifeSupportRating)
 }
 
+func getPowerConsumption(data []string) (int, error) {
+	gammaRate, epsilonRate, err := getGammaAndEpsilonRates(data)
+	if err != nil {
+		return 0, err
+	}
+	return mustConvertBinToDec(gammaRate) * mustConvertBinToDec(epsilonRate), nil
+}
+
+func getGammaAndEpsilonRates(data []string) (gammaRate string, epsilonRate string, err error) {
+	onesCounter, err := countOnesInAllPositions(data)
+	if err != nil {
+		return "", "", err
+	}
+
+	var (
+		gammaRateSB   strings.Builder
+		epsilonRateSB strings.Builder
+	)
+	for i := 0; i < len(onesCounter); i++ {
+		if onesCounter[i] > len(data)/2 {
+			gammaRateSB.WriteRune('1')
+			epsilonRateSB.WriteRune('0')
+			continue
+		}
+		gammaRateSB.WriteRune('0')
+		epsilonRateSB.WriteRune('1')
+	}
+	return gammaRateSB.String(), epsilonRateSB.String(), nil
+}
+
+func countOnesInAllPositions(data []string) (map[int]int, error) {
+	length := len(data[0])
+	onesCounter := make(map[int]int) // position - amount
+
+	for _, nextVal := range data {
+		if len(nextVal) != length {
+			return nil, fmt.Errorf("different length of data in report")
+		}
+		for i := 0; i < length; i++ {
+			if nextVal[i] == '1' {
+				onesCounter[i]++
+				continue
+			}
+			if nextVal[i] != '0' {
+				return nil, fmt.Errorf("not binary format in report %q", nextVal)
+			}
+		}
+	}
+	return onesCounter, nil
+}
+
 func getLifeSupportRating(data []string) (int, error) {
 	oxygenGeneratorRating, err := getOxygenGeneratorRating(data)
 	if err != nil {
@@ -51,16 +102,18 @@ func getCo2ScrubberRating(data []string) (string, error) {
 	return getRatingRec(data, 0, findLeastCommonBit)
 }
 
-// Co2ScrubberRating && OxygenGeneratorRating
+// getRatingRec for both Co2ScrubberRating && OxygenGeneratorRating recursively
 func getRatingRec(data []string, startPosition int, findCommonBitFunc func(data []string, position int) (rune, error)) (string, error) {
 	if len(data) == 1 {
 		return data[0], nil
 	}
+
 	mostCommon, err := findCommonBitFunc(data, startPosition)
 	if err != nil {
 		return "", err
 	}
-	filtered := []string{}
+
+	var filtered []string
 	for _, next := range data {
 		runes := []rune(next)
 		if runes[startPosition] == mostCommon {
@@ -70,9 +123,8 @@ func getRatingRec(data []string, startPosition int, findCommonBitFunc func(data 
 	return getRatingRec(filtered, startPosition+1, findCommonBitFunc)
 }
 
-// findMostCommonBit, position starts from 0
 func findMostCommonBit(data []string, position int) (rune, error) {
-	onesCount, err := countOnesOnPosition(data, position)
+	onesCount, err := countOnesInPosition(data, position)
 	if err != nil {
 		return '-', err
 	}
@@ -84,7 +136,7 @@ func findMostCommonBit(data []string, position int) (rune, error) {
 }
 
 func findLeastCommonBit(data []string, position int) (rune, error) {
-	onesCount, err := countOnesOnPosition(data, position)
+	onesCount, err := countOnesInPosition(data, position)
 	if err != nil {
 		return '-', err
 	}
@@ -94,8 +146,7 @@ func findLeastCommonBit(data []string, position int) (rune, error) {
 		return '0', nil
 	}
 }
-
-func countOnesOnPosition(data []string, position int) (int, error) {
+func countOnesInPosition(data []string, position int) (int, error) {
 	var onesCount int
 	for _, nextVal := range data {
 		if nextVal[position] == '1' {
@@ -107,60 +158,6 @@ func countOnesOnPosition(data []string, position int) (int, error) {
 		}
 	}
 	return onesCount, nil
-}
-
-// getPowerConsumption returns the result for the first part
-func getPowerConsumption(data []string) (int, error) {
-	onesCounter, err := countOnesInPositions(data)
-	if err != nil {
-		return 0, err
-	}
-
-	gammaRate, epsilonRate := getGammaAndEpsilonRates(onesCounter, len(data))
-
-	gammaRateDec := mustConvertBinToDec(gammaRate)
-	epsilonRateDec := mustConvertBinToDec(epsilonRate)
-
-	return gammaRateDec * epsilonRateDec, nil
-}
-
-func countOnesInPositions(data []string) (map[int]int, error) {
-	length := len(data[0])
-	onesCounter := make(map[int]int) // position - amount
-
-	for _, nextVal := range data {
-		if len(nextVal) != length {
-			return nil, fmt.Errorf("different length of data in report")
-		}
-		for i := 0; i < length; i++ {
-			if nextVal[i] == '1' {
-				onesCounter[i]++
-				continue
-			}
-			if nextVal[i] != '0' {
-				return nil, fmt.Errorf("not binary format in report %q", nextVal)
-			}
-		}
-	}
-
-	return onesCounter, nil
-}
-
-func getGammaAndEpsilonRates(onesCounter map[int]int, rows int) (gammaRate string, epsilonRate string) {
-	var (
-		gammaRateSB   strings.Builder
-		epsilonRateSB strings.Builder
-	)
-	for i := 0; i < len(onesCounter); i++ {
-		if onesCounter[i] > rows/2 {
-			gammaRateSB.WriteRune('1')
-			epsilonRateSB.WriteRune('0')
-			continue
-		}
-		gammaRateSB.WriteRune('0')
-		epsilonRateSB.WriteRune('1')
-	}
-	return gammaRateSB.String(), epsilonRateSB.String()
 }
 
 func mustConvertBinToDec(num string) int {
--- 2021/day3/binary_diagnostic_test.go
+++ 2021/day3/binary_diagnostic_test.go
@@ -6,109 +6,92 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGetLifeSupportRating(t *testing.T) {
-	tests := []struct {
-		name   string
-		input  []string
-		result int
-	}{
-		{
-			name: "test 1",
-			input: []string{
-				"00100",
-				"11110",
-				"10110",
-				"10111",
-				"10101",
-				"01111",
-				"00111",
-				"11100",
-				"10000",
-				"11001",
-				"00010",
-				"01010",
-			},
-			result: 230,
-		},
+func TestGetPowerConsumption(t *testing.T) {
+	input := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
 	}
+	expectedResult := 198
 
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			result, err := getLifeSupportRating(tc.input)
-			require.NoError(t, err)
-			require.Equal(t, tc.result, result)
-		})
-	}
+	actualResult, err := getPowerConsumption(input)
+	require.NoError(t, err)
+	require.Equal(t, expectedResult, actualResult)
 }
 
-func TestGetCo2ScrubberRating(t *testing.T) {
-	tests := []struct {
-		name   string
-		input  []string
-		result string
-	}{
-		{
-			name: "test 1",
-			input: []string{
-				"00100",
-				"11110",
-				"10110",
-				"10111",
-				"10101",
-				"01111",
-				"00111",
-				"11100",
-				"10000",
-				"11001",
-				"00010",
-				"01010",
-			},
-			result: "01010",
-		},
+func TestGetLifeSupportRating(t *testing.T) {
+	input := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
 	}
+	expectedResult := 230
 
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			result, err := getCo2ScrubberRating(tc.input)
-			require.NoError(t, err)
-			require.Equal(t, tc.result, result)
-		})
-	}
+	actualResult, err := getLifeSupportRating(input)
+	require.NoError(t, err)
+	require.Equal(t, expectedResult, actualResult)
 }
 
 func TestGetOxygenGeneratorRating(t *testing.T) {
-	tests := []struct {
-		name   string
-		input  []string
-		result string
-	}{
-		{
-			name: "test 1",
-			input: []string{
-				"00100",
-				"11110",
-				"10110",
-				"10111",
-				"10101",
-				"01111",
-				"00111",
-				"11100",
-				"10000",
-				"11001",
-				"00010",
-				"01010",
-			},
-			result: "10111",
-		},
+	input := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
 	}
+	expectedResult := "10111"
 
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			result, err := getOxygenGeneratorRating(tc.input)
-			require.NoError(t, err)
-			require.Equal(t, tc.result, result)
-		})
+	actualResult, err := getOxygenGeneratorRating(input)
+	require.NoError(t, err)
+	require.Equal(t, expectedResult, actualResult)
+}
+
+func TestGetCo2ScrubberRating(t *testing.T) {
+	input := []string{
+		"00100",
+		"11110",
+		"10110",
+		"10111",
+		"10101",
+		"01111",
+		"00111",
+		"11100",
+		"10000",
+		"11001",
+		"00010",
+		"01010",
 	}
+	expectedResult := "01010"
+
+	actualResult, err := getCo2ScrubberRating(input)
+	require.NoError(t, err)
+	require.Equal(t, expectedResult, actualResult)
 }
 
 func TestFindMostCommonBit(t *testing.T) {
@@ -203,28 +186,6 @@ func TestFindLeastCommonBit(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			result, err := findLeastCommonBit(tc.input, tc.position)
-			require.NoError(t, err)
-			require.Equal(t, tc.result, result)
-		})
-	}
-}
-
-func TestGetPowerConsumption(t *testing.T) {
-	tests := []struct {
-		name   string
-		input  []string
-		result int
-	}{
-		{
-			name:   "test 1",
-			input:  []string{"00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"},
-			result: 198,
-		},
-	}
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			result, err := getPowerConsumption(tc.input)
 			require.NoError(t, err)
 			require.Equal(t, tc.result, result)
 		})
