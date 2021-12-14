eb95ed0d79424f925c930f0fd7eabed1a34609a7
Lidiya Ribakova <lida.ribakova@gmail.com>
2021-12-14 07:28:05 +0100

    day13 refactoring

--- 2021/day13/transparent_origami_test.go
+++ 2021/day13/transparent_origami_test.go
@@ -2,49 +2,64 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetPointsAfterFirstFold(t *testing.T) {
-	tests := []struct {
-		name   string
-		input  []string
-		result int
-	}{
-		{
-			name: "test 0",
-			input: []string{
-				"6,10",
-				"0,14",
-				"9,10",
-				"0,3",
-				"10,4",
-				"4,11",
-				"6,0",
-				"6,12",
-				"4,1",
-				"0,13",
-				"10,12",
-				"3,4",
-				"3,0",
-				"8,4",
-				"1,10",
-				"2,14",
-				"8,10",
-				"9,0",
-				"",
-				"fold along y=7",
-				"fold along x=5",
-			},
-			result: 17,
-		},
+	input := []string{
+		"6,10",
+		"0,14",
+		"9,10",
+		"0,3",
+		"10,4",
+		"4,11",
+		"6,0",
+		"6,12",
+		"4,1",
+		"0,13",
+		"10,12",
+		"3,4",
+		"3,0",
+		"8,4",
+		"1,10",
+		"2,14",
+		"8,10",
+		"9,0",
+		"",
+		"fold along y=7",
+		"fold along x=5",
+	}
+	expectedResult := 17
+
+	actualResult := getPointsAfterFirstFold(input)
+	require.Equal(t, expectedResult, actualResult)
+}
+
+func TestPrintPointsAfterAllFolds(t *testing.T) {
+	input := []string{
+		"6,10",
+		"0,14",
+		"9,10",
+		"0,3",
+		"10,4",
+		"4,11",
+		"6,0",
+		"6,12",
+		"4,1",
+		"0,13",
+		"10,12",
+		"3,4",
+		"3,0",
+		"8,4",
+		"1,10",
+		"2,14",
+		"8,10",
+		"9,0",
+		"",
+		"fold along y=7",
+		"fold along x=5",
 	}
 
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			//result := getPointsAfterFirstFold(tc.input)
-			//require.Equal(t, tc.result, result)
-
-			printPointsAfterAllFolds(tc.input)
-		})
-	}
+	printPointsAfterAllFolds(input)
 }
