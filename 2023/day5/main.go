bebb63b9d536b5260d805506d1023d57413df5fb
Lidiya Ribakova <lida.ribakova@gmail.com>
2023-12-13 15:08:01 +0100

    day5

--- 2023/day5/main.go
+++ 2023/day5/main.go
@@ -5,7 +5,6 @@ import (
 	"advent-of-code/parse"
 	"advent-of-code/utils"
 	"fmt"
-	"sort"
 )
 
 func main() {
@@ -17,6 +16,104 @@ func main() {
 
 	fmt.Println("puzzle 1:", puzzle1(data))
 	fmt.Println("puzzle 2:", puzzle2(data))
+}
+
+func puzzle2(lines []string) int {
+	seedsFromFile, _ := parse.StringToInts(lines[0][7:], " ")
+	sourceArray := fromPairsToSourceRanges(seedsFromFile)
+
+	for i := 3; i < len(lines); {
+		mappingsLastLine := i
+		for ; mappingsLastLine < len(lines); mappingsLastLine++ {
+			if lines[mappingsLastLine] == "" {
+				break
+			}
+		}
+
+		sourceArray = mapRangesArray(sourceArray, lines[i:mappingsLastLine])
+		i = mappingsLastLine + 2
+	}
+
+	return getRangesMinimum(sourceArray)
+}
+
+func getRangesMinimum(sourceRange []sourceRange) int {
+	minimum := sourceRange[0].start
+	for _, source := range sourceRange {
+		if source.start < minimum {
+			minimum = source.start
+		}
+	}
+	return minimum
+}
+
+func fromPairsToSourceRanges(pairs []int) []sourceRange {
+	var newArr []sourceRange
+	for i := 0; i < len(pairs); i += 2 {
+		newArr = append(newArr, sourceRange{pairs[i], pairs[i] + pairs[i+1] - 1})
+	}
+	return newArr
+}
+
+func mapRangesArray(arraySource []sourceRange, mappingLines []string) (res []sourceRange) {
+	rules := fromMappingLinesToDestinationRangeMapping(mappingLines)
+
+	for i := 0; i < len(arraySource); i++ {
+		source := arraySource[i]
+		var updated bool
+		for _, rule := range rules {
+			upd, new, ok := applyRule(source, rule)
+			arraySource = append(arraySource, new...)
+			updated = updated || ok
+			if ok {
+				res = append(res, upd)
+				break
+			}
+		}
+		if !updated {
+			res = append(res, source)
+		}
+	}
+
+	return res
+}
+
+func applyRule(source sourceRange, rule rule) (sourceRange, []sourceRange, bool) {
+	var newSources []sourceRange
+	updatedSource := source
+	var updated bool
+
+	if source.start >= rule.start && source.end <= rule.end {
+		source.start += rule.shift
+		source.end += rule.shift
+		updatedSource = source
+		updated = true
+	}
+	if source.start < rule.start && source.end > rule.end {
+		newSources = []sourceRange{{source.start, rule.start - 1}, {rule.end + 1, source.end}}
+		updatedSource = sourceRange{rule.start + rule.shift, rule.end + rule.shift}
+		updated = true
+	}
+	if source.start < rule.start && source.end <= rule.end && source.end >= rule.start {
+		newSources = []sourceRange{{source.start, rule.start - 1}}
+		updatedSource = sourceRange{rule.start + rule.shift, source.end + rule.shift}
+		updated = true
+	}
+	if source.start >= rule.start && source.end > rule.end && source.start <= rule.end {
+		newSources = []sourceRange{{rule.end + 1, source.end}}
+		updatedSource = sourceRange{source.start + rule.shift, rule.end + rule.shift}
+		updated = true
+	}
+	return updatedSource, newSources, updated
+}
+
+func fromMappingLinesToDestinationRangeMapping(mappingLines []string) []rule {
+	var newArr []rule
+	for _, line := range mappingLines {
+		mapLine, _ := parse.StringToInts(line, " ")
+		newArr = append(newArr, rule{mapLine[1], mapLine[1] + mapLine[2] - 1, mapLine[0] - mapLine[1]})
+	}
+	return newArr
 }
 
 func puzzle1(lines []string) int {
@@ -62,128 +159,8 @@ type sourceRange struct {
 	end   int
 }
 
-type destinationRangeMapping struct {
+type rule struct {
 	start int
 	end   int
 	shift int
-}
-
-func puzzle2(lines []string) int {
-	seedsFromFile, _ := parse.StringToInts(lines[0][7:], " ")
-	sourceArray := fromPairsToSourceRanges(seedsFromFile)
-
-	for i := 3; i < len(lines); {
-		mappingsLastLine := i
-		for ; mappingsLastLine < len(lines); mappingsLastLine++ {
-			if lines[mappingsLastLine] == "" {
-				break
-			}
-		}
-
-		sourceArray = mapRangesArray(sourceArray, lines[i:mappingsLastLine])
-		i = mappingsLastLine + 2
-	}
-
-	return getRangesMinimum(sourceArray)
-}
-
-func fromPairsToSourceRanges(pairs []int) []sourceRange {
-	var newArr []sourceRange
-	for i := 0; i < len(pairs); i += 2 {
-		newArr = append(newArr, sourceRange{pairs[i], pairs[i] + pairs[i+1] - 1})
-	}
-	return newArr
-}
-
-func fromMappingLinesToDestinationRangeMapping(mappingLines []string) []destinationRangeMapping {
-	var newArr []destinationRangeMapping
-	for _, line := range mappingLines {
-		mapLine, _ := parse.StringToInts(line, " ")
-		newArr = append(newArr, destinationRangeMapping{mapLine[1], mapLine[1] + mapLine[2], mapLine[0] - mapLine[1]})
-	}
-	return newArr
-}
-
-func mapRangesArray(arraySource []sourceRange, mappingLines []string) []sourceRange {
-	destinationRangeMappings := fromMappingLinesToDestinationRangeMapping(mappingLines)
-
-	var rangesNoOverlap []sourceRange
-	for _, currentSource := range arraySource {
-		sourceStart := currentSource.start
-		sourceEnd := currentSource.end
-
-		var overlapFound bool
-
-		for _, mapping := range destinationRangeMappings {
-			destinationEnd := mapping.end
-			destinationStart := mapping.start
-
-			if sourceStart > destinationEnd || sourceEnd < destinationStart {
-				continue
-			}
-
-			overlapFound = true
-
-			intSlice := []int{sourceStart, sourceEnd, destinationEnd, destinationStart}
-			sort.Ints(intSlice)
-
-			rangesNoOverlap = append(rangesNoOverlap, sourceRange{intSlice[1], intSlice[2]})
-
-			if sourceStart > destinationStart && sourceEnd < destinationEnd {
-				continue
-			}
-			if destinationStart < sourceStart && destinationEnd < sourceEnd {
-				rangesNoOverlap = append(rangesNoOverlap, sourceRange{intSlice[2] + 1, intSlice[3]})
-				continue
-			}
-			if destinationEnd > sourceEnd && destinationStart > sourceStart {
-				rangesNoOverlap = append(rangesNoOverlap, sourceRange{intSlice[0], intSlice[1] - 1})
-				continue
-			}
-			rangesNoOverlap = append(rangesNoOverlap, sourceRange{intSlice[0], intSlice[1] - 1})
-			rangesNoOverlap = append(rangesNoOverlap, sourceRange{intSlice[2] + 1, intSlice[3]})
-			continue
-		}
-
-		if !overlapFound {
-			rangesNoOverlap = append(rangesNoOverlap, currentSource)
-		}
-	}
-
-	var newSource []sourceRange
-
-	for _, currentSource := range rangesNoOverlap {
-		sourceStart := currentSource.start
-		sourceEnd := currentSource.end
-
-		var overlapFound bool
-
-		for _, mapping := range destinationRangeMappings {
-			destinationEnd := mapping.end
-			destinationStart := mapping.start
-
-			if sourceStart > destinationEnd || sourceEnd < destinationStart {
-				continue
-			}
-
-			overlapFound = true
-			newSource = append(newSource, sourceRange{sourceStart + mapping.shift, sourceEnd + mapping.shift})
-			break
-		}
-
-		if !overlapFound {
-			newSource = append(newSource, currentSource)
-		}
-	}
-	return newSource
-}
-
-func getRangesMinimum(sourceRange []sourceRange) int {
-	minimum := sourceRange[0].start
-	for _, source := range sourceRange {
-		if source.start < minimum {
-			minimum = source.start
-		}
-	}
-	return minimum
 }
