606352d787e648337662256f2e67c0e8a528343f
Lidiya Ribakova <lida.ribakova@gmail.com>
2023-12-14 22:08:55 +0100

    day9

---
+++ 2023/day9/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"advent-of-code/file"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := file.ParseToStrings("2023/day9/input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("puzzle 1:", puzzle1(data))
+	fmt.Println("puzzle 2:", puzzle2(data))
+}
+
+func puzzle1(lines []string) int {
+	var res int
+
+	for _, line := range lines {
+		numbersStr := strings.Split(line, " ")
+
+		var differences [][]int
+
+		var zeroLevel []int
+		for _, numberStr := range numbersStr {
+			number, _ := strconv.Atoi(numberStr)
+			zeroLevel = append(zeroLevel, number)
+		}
+		differences = append(differences, zeroLevel)
+
+		for j := 0; ; j++ {
+			allZeros := true
+			var nextLevelDiferences []int
+			for i := 0; i < len(differences[j])-1; i++ {
+				diff := differences[j][i+1] - differences[j][i]
+				nextLevelDiferences = append(nextLevelDiferences, diff)
+
+				if diff != 0 {
+					allZeros = false
+				}
+			}
+			differences = append(differences, nextLevelDiferences)
+			if allZeros {
+				break
+			}
+		}
+
+		differences[len(differences)-1] = append(differences[len(differences)-1], 0)
+
+		for i := len(differences) - 2; i >= 0; i-- {
+			nextLineLastItem := differences[i+1][len(differences[i+1])-1]
+			currentLineLastItem := differences[i][len(differences[i])-1]
+			differences[i] = append(differences[i], nextLineLastItem+currentLineLastItem)
+		}
+
+		res += differences[0][len(differences[0])-1]
+	}
+
+	return res
+}
+
+func puzzle2(lines []string) int {
+	var res int
+
+	for _, line := range lines {
+		numbersStr := strings.Split(line, " ")
+
+		var differences [][]int
+
+		zeroLevel := make([]int, len(numbersStr))
+		for i, numberStr := range numbersStr {
+			number, _ := strconv.Atoi(numberStr)
+			zeroLevel[len(numbersStr)-i-1] = number
+		}
+		differences = append(differences, zeroLevel)
+
+		for j := 0; ; j++ {
+			allZeros := true
+			var nextLevelDiferences []int
+			for i := 0; i < len(differences[j])-1; i++ {
+				diff := differences[j][i+1] - differences[j][i]
+				nextLevelDiferences = append(nextLevelDiferences, diff)
+
+				if diff != 0 {
+					allZeros = false
+				}
+			}
+			differences = append(differences, nextLevelDiferences)
+			if allZeros {
+				break
+			}
+		}
+
+		differences[len(differences)-1] = append(differences[len(differences)-1], 0)
+
+		for i := len(differences) - 2; i >= 0; i-- {
+			nextLineLastItem := differences[i+1][len(differences[i+1])-1]
+			currentLineLastItem := differences[i][len(differences[i])-1]
+			differences[i] = append(differences[i], nextLineLastItem+currentLineLastItem)
+		}
+
+		res += differences[0][len(differences[0])-1]
+	}
+
+	return res
+}
---
+++ 2023/day9/main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPuzzle1(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []string
+		expected int
+	}{
+		{
+			name: "test_1",
+			input: []string{
+				"0 3 6 9 12 15",
+				"1 3 6 10 15 21",
+				"10 13 16 21 30 45",
+			},
+			expected: 114,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result := puzzle1(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestPuzzle2(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []string
+		expected int
+	}{
+		{
+			name: "test_1",
+			input: []string{
+				"0 3 6 9 12 15",
+				"1 3 6 10 15 21",
+				"10 13 16 21 30 45",
+			},
+			expected: 2,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result := puzzle2(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
